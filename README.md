ac50ef11d9970f3b6424c1895b5a7e3b222502ea
mobbing <ifthe1201@naver.com>
2019-12-10 22:19:02 +0900

    🇰🇷 Parsing Arguments, Add option [ no messages, exclude, exclude dir ]

--- README.md
+++ README.md
@@ -9,6 +9,17 @@ cd ngrep
 npm install
 ```
 
+Compile ngrep.ts
+``` bash
+npm run compile
+```
+
+Compile and Run ngrep.js
+``` bash
+npm run start
+```
+
+
 You should have [nodejs](https://nodejs.org/ko/download/package-manager/) at least 8.x.
 
 ## How to use
@@ -18,6 +29,14 @@ Help:
 Usage: ngrep [search] [file] [options...]
        search: javascript regex pattern string for search.
        file:   file pattern javascript regex string
+
+Options
+  -i, --ignore-case       Ignore case distinctions in both the PATTERN and the input files.
+  -r, --recursive         Read  all  files  under each directory.
+  -n, --line-number       Prefix each line of output with the 1-based line number within its input file.
+  -s, --no-messages       Suppress error messages.
+  --exclude FILE_PATTERN  skip files and directories matching FILE_PATTERN
+  --exclude-dir PATTERN   directories that match PATTERN will be skipped.
 ```
 
 Default:
--- ngrep.ts
+++ ngrep.ts
@@ -7,48 +7,79 @@ const bg_color = (c:Number) => `\x1b[48;5;${c}m`;
 const BOLD = "\x1b[1m";
 const RESET = "\x1b[0;0;0m";
 
+let searchPattern = null; 
+let filePattern = [];
+let readStream = null;
+
 const color = (str: String, fg: Number, bg: Number = 0) => {
 	return fg_color(fg) + bg_color(bg) + str + RESET;
 };
 
-const options = {
+const g_options = {
 	"ignore-case" : false,
 	"recursive"   : false,
-	"line-number" : false
+	"line-number" : false,
+	"no-messages" : false,
+	"exclude"     : null,
+	"exclude-dir" : null 
+};
+
+const errorMsg = (msg: String) => {
+	if ( g_options['no-messages'] === false ) {
+		console.error(msg);
+	}
 };
 
 const printErrorExit = (msg: String) => {
-	console.error(msg, "\n");
-	console.error("Usage: ngrep [search] [file] [options...]");
-	console.error("       search: javascript regex pattern string for search.");
-	console.error("       file:   file pattern javascript regex string");
-	console.error("");
-	console.error("Options");
-	console.error("  -i, --ignore-case       Ignore case distinctions in both the PATTERN and the input files.");
-	console.error("  -r, --recursive         Read  all  files  under each directory.");
-	console.error("  -n, --line-number       Prefix each line of output with the 1-based line number within its input file.");
+	errorMsg(msg+"\n");
+	errorMsg("Usage: ngrep [search] [file] [options...]");
+	errorMsg("       search: javascript regex pattern string for search.");
+	errorMsg("       file:   file pattern javascript regex string");
+	errorMsg("");
+	errorMsg("Options");
+	errorMsg("  -i, --ignore-case       Ignore case distinctions in both the PATTERN and the input files.");
+	errorMsg("  -r, --recursive         Read  all  files  under each directory.");
+	errorMsg("  -n, --line-number       Prefix each line of output with the 1-based line number within its input file.");
+	errorMsg("  -s, --no-messages       Suppress error messages.");
+	errorMsg("  --exclude FILE_PATTERN  skip files and directories matching FILE_PATTERN");
+	errorMsg("  --exclude-dir PATTERN   directories that match PATTERN will be skipped.");
 	process.exit();
 };
 
 const parsingArgv = (argv: Array<String>) => {
 	let i = 0, len = argv.length;
 	for (i=0;i < len;i++) {
-		switch ( argv[i] ) {
-			case "-i":
-			case "--ignore-case":
-				options["ignore-case"] = true;
-				break;
-			case "-r":
-			case "--recursive":
-				options["recursive"] = true;
-				break;
-			case "-n":
-			case "--line-number":
-				options["line-number"] = true;
-				break;
-			default:
-				if ( argv[i][0] === "-" )
-					printErrorExit(`Invalid options ${argv[i]}`);
+		if ( argv[i][0] === "-" ) {
+			if ( argv[i][1] && argv[i][1] === "-" ) {
+				// --[option]
+				switch ( argv[i] ) {
+					case "--ignore-case": g_options['ignore-case'] = true; break;
+					case "--recursive": g_options['recursive'] = true; break;
+					case "--line-number": g_options['line-number'] = true; break;
+					case "--no-messages": g_options['no-messages'] = true; break;
+					case "--exclude": g_options['exclude'] = argv[++i]; break;
+					case "--exclude-dir" : g_options['exclude-dir'] = argv[++i]; break;
+					default: printErrorExit('Invalid Options');
+				}
+			} else {
+				// -[options]
+				let len = argv[i].length;
+				for (let idx = 1;idx < len;idx++) {
+					switch ( argv[i][idx] ) {
+						case "i": g_options['ignore-case'] = true; break;
+						case "r": g_options['recursive'] = true; break;
+						case "n": g_options['line-number'] = true; break;
+						case "s": g_options['no-messages'] = true; break;
+						default: printErrorExit('Invalid Options');
+					}
+				}
+			}
+		} else {
+			if ( searchPattern === null ) {
+				searchPattern = argv[i];
+			} else {
+				filePattern.push(argv[i]);
+			}
 		}
 	}
 };
@@ -95,34 +126,59 @@ const realRunGrep = (file: string, searchPattern: string, options: Object) => {
 	});
 };
 
-const grep = (pattern: string, searchPattern: string, path: String, options: Object) => {
-	if ( pattern === undefined ) return;
-	let fRegex = new RegExp(pattern, 'i');
-	const matchFile = (_path: String, _recursive: Boolean) => {
-		let dirs = fs.readdirSync(_path as fs.PathLike);
-		let retDirs = [];
-		dirs.forEach(d => {
-			let target = pathJoin(_path, d);
-			let stat = fs.lstatSync(target as fs.PathLike);
-			if ( stat.isDirectory() ) {
-				retDirs.push(target);
-			} else {
-				if ( d.match(fRegex) ) {
-					// real run grep
-					realRunGrep(target, searchPattern, options);
+const matchFile = (fRegex: RegExp, path: String, recursive: Boolean) => {
+	let dirs = fs.readdirSync(path as fs.PathLike);
+	let retDirs = [];
+	dirs.forEach(d => {
+		let target = pathJoin(path, d);
+		let stat = fs.lstatSync(target as fs.PathLike);
+		if ( stat.isDirectory() ) {
+			let excludeDir = g_options['exclude-dir'];
+			let isExclude = false;
+
+			if ( excludeDir ) {
+				let eRegex = new RegExp(excludeDir, 'i');
+				if ( d.match(eRegex) ) {
+					isExclude = true;
 				}
 			}
 
-			if ( _recursive ) {
-				retDirs.forEach(r => {
-					matchFile(r, _recursive);
-				});
+			if ( isExclude === false ) {
+				retDirs.push(target);
 			}
+		} else {
+			let exclude = g_options['exclude'];
+			let isExclude = false;
+
+			if ( exclude ) {
+				let eRegex = new RegExp(exclude, 'i');
+				if ( d.match(eRegex) ) {
+					isExclude = true;
+				}
+			}
+
+			if ( isExclude === false ) {
+				if ( d.match(fRegex) ) {
+					// real run grep
+					realRunGrep(target, searchPattern, g_options);
+				}
+			}
+		}
+	});
+
+	if ( recursive ) {
+		retDirs.forEach(r => {
+			matchFile(fRegex, r, recursive);
 		});
-		return retDirs;
-	};
-	return new Promise((resolve, reject) => {
-		resolve(matchFile(path, options['recursive']) as Array<String>);
+	}
+	return retDirs;
+};
+
+const grep = (pattern: Array<string>, searchPattern: string, path: String, options: Object) => {
+	if ( pattern === undefined ) return;
+	pattern.forEach(p => {
+		let fRegex = new RegExp(p, 'i');
+		matchFile(fRegex, path, options['recursive']);
 	});
 };
 
@@ -131,10 +187,7 @@ let argv = process.argv.splice(2);
 if ( argv.length < 1 ) {
 	printErrorExit("There are few arguments.");
 }
-
-let searchPattern = argv[0];
-let filePattern = argv[1];
-let readStream = null;
+parsingArgv(argv);
 
 let regex = new RegExp(searchPattern, 'g');
 
@@ -143,13 +196,13 @@ if ( filePattern === undefined ) {
 	readStream.on('data', (chunk: String) => {
 		let str = chunk.toString();
 		str.split('\n').forEach(c => {
-			let m = matchingLine(c, searchPattern, options);
+			let m = matchingLine(c, searchPattern, g_options);
 			if ( m && m.length > 0 ) {
 				console.log(m);
 			}
 		});
 	});
 } else {
-	grep(filePattern, searchPattern, './', options);
+	grep(filePattern, searchPattern, './', g_options);
 }
 
